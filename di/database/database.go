d27dbefbaa8b8aa92fdbc4e30536b01d2281dbd3
algnot <thanawat.tongla@gmail.com>
2025-09-06 21:23:01 +0700

    fix up

--- di/database/database.go
+++ di/database/database.go
@@ -14,7 +14,7 @@ import (
 func InitDatabase() (*gorm.DB, error) {
 	DBConfig := getDatabaseConfig()
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true&loc=Local",
 		DBConfig.User, DBConfig.Password, DBConfig.Host, DBConfig.Port, DBConfig.DBName)
 
 	sqlDB, err := sql.Open("mysql", dsn)
